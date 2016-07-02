42ebdd797abeb0071ce6fe9661a7a10c81e36290
Maxime Rignault <maximerignault@gmail.com>
2016-07-02 20:08:15 +0200

    Bugfixed players HUD by clearing skybox on background

--- Unity/Assets/scripts/SceneManager/SCRIPT_NetworkManager.cs
+++ Unity/Assets/scripts/SceneManager/SCRIPT_NetworkManager.cs
@@ -12,7 +12,6 @@ public class SCRIPT_NetworkManager : MonoBehaviour {
     {
         if(Input.GetKeyDown(KeyCode.Escape))
         {
-            NetworkManager.Shutdown();
             Destroy(NetworkManagerGO);
             SceneManager.LoadScene("MainMenu");
         }
